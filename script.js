8aba41605a070e4c0e5d5ee9571d0682cf32c829
N3rd <landonmarting@icloud.com>
2024-12-22 16:05:23 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,45 @@
-function generateMarkovChain(text) {
-    const Numbers = text.split(/\s+/);
-    const markovChain = {};
+// Transition probability matrix
+const P = [
+    [0, 0.75, 0, 0.25, 0],  // Transitions from state 1
+    [0.5, 0, 1, 0, 0],      // Transitions from state 2
+    [0, 0.33, 0, 0.33, 0.33], // Transitions from state 3
+    [0, 0, 0, 0, 1],        // Transitions from state 4
+    [0, 0, 0, 0, 0]         // Transitions from state 5
+];
 
-    for (let i = 0; i < Numbers.length - 1; i++) {
-        const Number = Numbers[i];
-        const nextNumber = Numbers[i + 1];
+// State space
+const states = [1, 2, 3, 4, 5];
 
-        if (!markovChain[Number]) {
-            markovChain[Number] = [];
+// Function to simulate the Markov chain
+function simulateMarkovChain(startState, steps) {
+    let currentState = startState;
+    const stateSequence = [currentState];
+
+    for (let i = 0; i < steps; i++) {
+        const currentStateIndex = states.indexOf(currentState);
+        const probabilities = P[currentStateIndex];
+
+        // Generate a random number between 0 and 1
+        const randomNum = Math.random();
+        let cumulativeProbability = 0;
+
+        // Determine the next state based on the probabilities
+        for (let j = 0; j < probabilities.length; j++) {
+            cumulativeProbability += probabilities[j];
+            if (randomNum < cumulativeProbability) {
+                currentState = states[j];
+                break;
+            }
         }
-        markovChain[Number].push(nextNumber);
+
+        stateSequence.push(currentState);
     }
 
-    let currentNumber = Numbers[Math.floor(Math.random() * Numbers.length)];
-    let result = currentNumber;
-
-    for (let i = 0; i < 50; i++) { // Generate 50 Numbers
-        const nextNumbers = markovChain[currentNumber];
-        if (!nextNumbers) break;
-        currentNumber = nextNumbers[Math.floor(Math.random() * nextNumbers.length)];
-        result += ' ' + currentNumber;
-    }
-    document.getElementById("output").innerHTML = "Hello, World!";
-    return result;
+    return stateSequence;
 }
+
+// Simulate the Markov chain
+const initialState = 1; // Starting state
+const steps = 10;       // Number of steps to simulate
+const result = simulateMarkovChain(initialState, steps);
+console.log("Simulated Markov Chain:", result);
